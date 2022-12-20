b9beb468f0cd4ca0c40ec159f5ae3d1cbe77ef9d
rodrigopontes <rodi.pontes@outlook.com>
2022-12-20 00:51:13 -0300

    ArrayList

---
+++ ByteBank/ByteBank/Help/ArrayListSample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace ByteBank.Help
+{
+    public class ArrayListSample
+    {
+        public ArrayList Items = new ArrayList();
+
+        public ArrayListSample()
+        {
+            Items.Add("An ArrayList can be used without knowing its items data types.");
+            Items.Add(50.2);
+            Items.Add(true);
+            Items.RemoveAt(1);
+            Items.IndexOf(50.2);
+            Items.Clear();
+        }
+    }
+}
