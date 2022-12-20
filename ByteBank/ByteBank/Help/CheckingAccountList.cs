05b6001e91182bd8a11962b0ff73d66f93111bcd
rodrigopontes <rodi.pontes@outlook.com>
2022-12-19 23:58:23 -0300

    CheckingAccountManager with Indexer

---
+++ ByteBank/ByteBank/Help/CheckingAccountList.cs
@@ -0,0 +1,109 @@
+﻿using ByteBank.AccountTypes;
+
+namespace ByteBank.Help
+{
+    public class CheckingAccountList
+    {
+        private int _currentIndex;
+
+        public CheckingAccount[] Accounts { get; private set; }
+
+        // Indexer
+        public CheckingAccount this[int index]
+        {
+            get
+            {
+                return GetAccount(index);
+            }
+        }
+
+
+        public CheckingAccountList()
+        {
+            Accounts = new CheckingAccount[0];
+        }
+
+
+        public void AddAccount(CheckingAccount account)
+        {
+            ResizeArray();
+            Accounts[_currentIndex] = account;
+            _currentIndex++;
+
+            Console.WriteLine($"Account {account.AccountNumber} has been added.");
+        }
+
+
+        public void RemoveAccount(CheckingAccount account)
+        {
+            int indexToRemove = -1;
+            
+            for (int i = 0; i < Accounts.Length; i++)
+            {
+                if (Accounts[i] == account)
+                {
+                    indexToRemove = i;
+                    break;
+                }
+            }
+
+            if (indexToRemove >= 0)
+            {
+                _currentIndex--;
+                CheckingAccount[] newAccountList = new CheckingAccount[_currentIndex];
+
+                int j = 0;
+                for (int i = 0; i < _currentIndex; i++)
+                {
+                    if (j == indexToRemove) j++;
+
+                    newAccountList[i] = Accounts[j++];
+                }
+
+                Accounts = newAccountList;
+            }
+
+            Console.WriteLine("The account has been removed.");
+        }
+
+
+        public CheckingAccount GetAccount(int index)
+        {
+            if (index < 0 || index > _currentIndex)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            return Accounts[index];
+        }
+
+
+        public void ResizeArray()
+        {
+            CheckingAccount[] newArray = new CheckingAccount[_currentIndex+1];
+
+            for (int i = 0; i < Accounts.Length; i++)
+            {
+                newArray[i] = Accounts[i];
+            }
+
+            Accounts = newArray;
+
+            Console.WriteLine($"The array has been resized to {_currentIndex + 1}.");
+        }
+
+
+        public void ListAccounts()
+        {
+            Console.WriteLine("\nList of accounts:");
+
+            foreach (CheckingAccount account in Accounts)
+            {
+                Console.WriteLine(account.AccountNumber);
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ByteBank.AccountTypes;
+using ByteBank.Help;
 using ByteBank.Screens;
 
 namespace ByteBank
@@ -7,22 +8,39 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            #region
             // ByteBank main app
-            //CheckingAccount account = new (1033, "1234-X", 312.30, "Andrew Hawkins", "32445211135", "Developer");
-            //CheckingAccount accountReceiver = new (2598, "2020-5", 4050.95, "Mary Antoniette", "876533562211", "Musician");
-            
+            CheckingAccount account = new(1033, "1234-X", 312.30, "Andrew Hawkins", "32445211135", "Developer");
+            CheckingAccount accountReceiver = new(2598, "2020-5", 4050.95, "Mary Antoniette", "876533562211", "Musician");
+
+            #region
             //StartScreen screen = new (account, accountReceiver);
-
             //screen.DisplayOptions();
+            #endregion
 
+            #region
             // Properties usage sample
             //CheckingAccount accountAnotherClient = new(4455, "9472-3", 20000000000.00, "Thiago Lacerda", "557788331122", "Actor");
             //accountAnotherClient.AccountNumber = "3441-2";
             #endregion
 
-            WordFinderScreen screen = new();
-            screen.DisplayOptions();
+            #region
+            //WordFinderScreen screen = new();
+            //screen.DisplayOptions();
+            #endregion
+
+            #region
+            // Resizeable array
+            CheckingAccountList accountManager = new();
+            accountManager.AddAccount(account);
+            accountManager.AddAccount(accountReceiver);
+            accountManager.ListAccounts();
+            accountManager.RemoveAccount(account);
+            accountManager.ListAccounts();
+
+            // Indexer
+            Console.WriteLine($"This is accessed through an indexer: {accountManager[0].AccountNumber}");
+
+            #endregion
         }
     }
 }
