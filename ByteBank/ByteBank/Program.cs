866e826b7746851e9978e03128bcdc494f93da74
rodrigopontes <rodi.pontes@outlook.com>
2022-12-20 00:21:39 -0300

    #Region and #Endregion

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -8,39 +8,34 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            // ByteBank main app
             CheckingAccount account = new(1033, "1234-X", 312.30, "Andrew Hawkins", "32445211135", "Developer");
             CheckingAccount accountReceiver = new(2598, "2020-5", 4050.95, "Mary Antoniette", "876533562211", "Musician");
 
-            #region
+            #region ByteBank Main App 
             //StartScreen screen = new (account, accountReceiver);
             //screen.DisplayOptions();
             #endregion
-
-            #region
-            // Properties usage sample
+            #region Properties usage sample
             //CheckingAccount accountAnotherClient = new(4455, "9472-3", 20000000000.00, "Thiago Lacerda", "557788331122", "Actor");
             //accountAnotherClient.AccountNumber = "3441-2";
             #endregion
-
-            #region
+            #region Word Finder - Arrays
             //WordFinderScreen screen = new();
             //screen.DisplayOptions();
             #endregion
+            #region Resizeable array with Indexer
+            //CheckingAccountList accountManager = new();
+            //accountManager.AddAccount(account);
+            //accountManager.AddAccount(accountReceiver);
+            //accountManager.ListAccounts();
+            //accountManager.RemoveAccount(account);
+            //accountManager.ListAccounts();
 
-            #region
-            // Resizeable array
-            CheckingAccountList accountManager = new();
-            accountManager.AddAccount(account);
-            accountManager.AddAccount(accountReceiver);
-            accountManager.ListAccounts();
-            accountManager.RemoveAccount(account);
-            accountManager.ListAccounts();
-
-            // Indexer
-            Console.WriteLine($"This is accessed through an indexer: {accountManager[0].AccountNumber}");
-
+            //// Indexer
+            //Console.WriteLine($"This is accessed through an indexer: {accountManager[0].AccountNumber}");
             #endregion
+
+
         }
     }
 }
